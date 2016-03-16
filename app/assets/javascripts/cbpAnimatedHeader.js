78838056f95128a552935e4c761f66eee30b3e23
nikthequik <quik.the.nick@gmail.com>
2016-03-16 13:02:49 -0600

    -convert cbpanimatedheader.js into jquery.  still getting null error for elem

--- app/assets/javascripts/cbpAnimatedHeader.js
+++ app/assets/javascripts/cbpAnimatedHeader.js
@@ -8,40 +8,13 @@
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
- 
-	var cbpAnimatedHeader = (function() {
-
-	var docElem = document.documentElement,
-		header = document.querySelector( '.navbar-fixed-top' ),
-		didScroll = false,
-		changeHeaderOn = 300;
-
-	function init() {
-		window.addEventListener( 'scroll', function( event ) {
-			if( !didScroll ) {
-				didScroll = true;
-				setTimeout( scrollPage, 250 );
-			}
-		}, false );
-	}
-
-	function scrollPage() {
-		var sy = scrollY(),
-		header = document.querySelector('.navbar-fixed-top' );
-
-		if ( sy >= changeHeaderOn ) {
-			classie.add(header, 'navbar-shrink' );
-		}
-		else {
-			classie.remove(header, 'navbar-shrink' );
-		}
-		didScroll = false;
-	}
-
-	function scrollY() {
-		return window.pageYOffset || docElem.scrollTop;
-	}
-
-	init();
-
+$(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 300){
+            $('.navbar-default').addClass('navbar-shrink');
+        }
+        else{
+            $('.navbar-default').removeClass('navbar-shrink');
+        }
+    });
 });
