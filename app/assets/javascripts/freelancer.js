655c1eb2ba7d40de9deeb4ab677a1ff19be761aa
nikthequik <quik.the.nick@gmail.com>
2016-03-19 13:51:56 -0600

    -moving jquery.js from end of body to end of head removed error

--- app/assets/javascripts/freelancer.js
+++ app/assets/javascripts/freelancer.js
@@ -35,4 +35,5 @@ $('body').scrollspy({
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
+    $('navbar-collapse').collapse('hide');
 });
